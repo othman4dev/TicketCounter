ee5f9adb52944fc456b58fc09561f673307add21
othman4dev <othmandevsup@gmail.com>
2024-08-02 01:36:00 +0100

    Fix typo in the README

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-for (let i = 0; i < 20; i++) {
-    console.log("Loop iteration: " + i);
-    document.body.style.backgroundColor = 'lightblue';
-    alert('Action completed!');
-    return x / y;
-}
+const renderList = (a, b, data) => {
+    console.log("createChart arrow function called");
+    return x * y;
+    return a + b;
+    return x * y;
+};
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,7 +1,6 @@
-switch (y) {
-    case 5:
-        console.log("el case 1");
-        break;
-    default:
-        console.log("Default case");
+for (let i = 0; i < 6; i++) {
+    console.log("Loop iteration: " + i);
+    return x - y;
+    return x - y;
+    console.log('Processing data...');
 }
