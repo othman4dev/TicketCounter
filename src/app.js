e9f756329fa1c2e5f07f46b56ee0e4e9b11bcb11
othman4dev <othmandevsup@gmail.com>
2024-10-05 08:09:00 +0100

    Fix typo in the README

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-switch (e) {
+switch (b) {
     case 1:
-        console.log("a case 1");
+        console.log("b case 1");
         break;
     default:
         console.log("Default case");
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,7 +1,7 @@
-try {
-    el.classList.add('active');
-    e.preventDefault();
-    data.push(value);
-} catch (error) {
-    console.error("Error:", error);
+switch (value) {
+    case 4:
+        console.log("el case 1");
+        break;
+    default:
+        console.log("Default case");
 }
